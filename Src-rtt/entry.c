d3004659e4a57fd06cd3fc4ff9749a6d051cd517
SuWeipeng <weipeng.su@outlook.com>
2019-11-25 15:04:33 +0800

    注释掉与 rt-thread 重复的 SDIO 初始化函数（否则上电启动超级慢）

--- Src-rtt/entry.c
+++ Src-rtt/entry.c
@@ -62,7 +62,7 @@ PCD_HandleTypeDef hpcd_USB_OTG_FS;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_SDIO_SD_Init(void);
+//static void MX_SDIO_SD_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_TIM3_Init(void);
@@ -109,7 +109,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_SDIO_SD_Init();
+//  MX_SDIO_SD_Init();
   MX_SPI1_Init();
   MX_TIM1_Init();
   MX_TIM3_Init();
@@ -184,36 +184,36 @@ void SystemClock_Config(void)
   * @param None
   * @retval None
   */
-static void MX_SDIO_SD_Init(void)
-{
-
-  /* USER CODE BEGIN SDIO_Init 0 */
-
-  /* USER CODE END SDIO_Init 0 */
-
-  /* USER CODE BEGIN SDIO_Init 1 */
-
-  /* USER CODE END SDIO_Init 1 */
-  hsd.Instance = SDIO;
-  hsd.Init.ClockEdge = SDIO_CLOCK_EDGE_RISING;
-  hsd.Init.ClockBypass = SDIO_CLOCK_BYPASS_DISABLE;
-  hsd.Init.ClockPowerSave = SDIO_CLOCK_POWER_SAVE_DISABLE;
-  hsd.Init.BusWide = SDIO_BUS_WIDE_1B;
-  hsd.Init.HardwareFlowControl = SDIO_HARDWARE_FLOW_CONTROL_DISABLE;
-  hsd.Init.ClockDiv = 0;
-  if (HAL_SD_Init(&hsd) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_SD_ConfigWideBusOperation(&hsd, SDIO_BUS_WIDE_4B) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN SDIO_Init 2 */
-
-  /* USER CODE END SDIO_Init 2 */
-
-}
+//static void MX_SDIO_SD_Init(void)
+//{
+//
+//  /* USER CODE BEGIN SDIO_Init 0 */
+//
+//  /* USER CODE END SDIO_Init 0 */
+//
+//  /* USER CODE BEGIN SDIO_Init 1 */
+//
+//  /* USER CODE END SDIO_Init 1 */
+//  hsd.Instance = SDIO;
+//  hsd.Init.ClockEdge = SDIO_CLOCK_EDGE_RISING;
+//  hsd.Init.ClockBypass = SDIO_CLOCK_BYPASS_DISABLE;
+//  hsd.Init.ClockPowerSave = SDIO_CLOCK_POWER_SAVE_DISABLE;
+//  hsd.Init.BusWide = SDIO_BUS_WIDE_1B;
+//  hsd.Init.HardwareFlowControl = SDIO_HARDWARE_FLOW_CONTROL_DISABLE;
+//  hsd.Init.ClockDiv = 0;
+//  if (HAL_SD_Init(&hsd) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  if (HAL_SD_ConfigWideBusOperation(&hsd, SDIO_BUS_WIDE_4B) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  /* USER CODE BEGIN SDIO_Init 2 */
+//
+//  /* USER CODE END SDIO_Init 2 */
+//
+//}
 
 /**
   * @brief SPI1 Initialization Function
